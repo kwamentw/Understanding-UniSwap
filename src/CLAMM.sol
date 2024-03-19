1ba11ba38fc9d22427d8d7ea88f83a087f0dd74e
KWAME <antwi1496@hotmail.com>
2024-03-19 11:23:28 -0500

    Writing, Deploying and Testing Contracts

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {Tick} from "./lib/Tick.sol";
+
 contract Clamm {
-    constructor() {}
+    address public immutable token0;
+    address public immutable token1;
+    uint24 public immutable fee;
+    int24 public immutable tickSpacing;
+    uint128 public immutable maxLiquidityPerTick;
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        int24 _tickSpacing
+    ) {
+        token0 = _token0;
+        token1 = _token1;
+        fee = _fee;
+        tickSpacing = _tickSpacing;
+        maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(
+            _tickSpacing
+        );
+    }
 }
---
+++ src/lib/Tick.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(
+        int24 tickSpacing
+    ) internal pure returns (uint128) {}
+}
