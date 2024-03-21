cc56ddd49f54bf97622517e9c607731bb68f4c75
KWAME <antwi1496@hotmail.com>
2024-03-20 20:36:18 -0500

    Writing, Deploying and Testing Contracts

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.20;
 
 import {Tick} from "./lib/Tick.sol";
--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.20;
 
 import {TickMath} from "./TickMath.sol";
@@ -9,7 +9,7 @@ library Tick {
     ) internal pure returns (uint128) {
         int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
         int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
-        uint24 numTicks = uint24(maxTick - minTick) / tickSpacing + 1;
+        uint24 numTicks = uint24((maxTick - minTick) / tickSpacing) + 1;
         return type(uint128).max / numTicks;
     }
 }
