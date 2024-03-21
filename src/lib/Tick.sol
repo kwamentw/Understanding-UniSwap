64a899bf8d855f7f2c14a04053cd8cda439886f1
KWAME <antwi1496@hotmail.com>
2024-03-20 20:24:31 -0500

    Writing, Deploying and Testing Contracts

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {TickMath} from "./TickMath.sol";
+
 library Tick {
     function tickSpacingToMaxLiquidityPerTick(
         int24 tickSpacing
-    ) internal pure returns (uint128) {}
+    ) internal pure returns (uint128) {
+        int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+        int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+        uint24 numTicks = uint24(maxTick - minTick) / tickSpacing + 1;
+        return type(uint128).max / numTicks;
+    }
 }
