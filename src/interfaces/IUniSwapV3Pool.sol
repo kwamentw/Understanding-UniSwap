8055a01ad4e95a884c379365f795155e3aa1e934
KWAME <antwi1496@hotmail.com>
2024-03-28 07:14:58 -0500

    Just a clone & test Repo

--- src/interfaces/IUniSwapV3Pool.sol
+++ src/interfaces/IUniSwapV3Pool.sol
@@ -12,4 +12,12 @@ interface IUniswapV3Pool {
         uint256 amount1,
         bytes calldata data
     ) external;
+
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1);
 }
